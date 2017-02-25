803f8e4bc07186061b69c1a51c97287ee84a4022
quiaro <quiaro@gmail.com>
2017-02-24 19:48:14 -0600

    Fetch posts created by user and by others

--- app/blog.py
+++ app/blog.py
@@ -62,7 +62,11 @@ class Blog(AppSection):
         ]
 
     def home(self):
+        my_posts = BlogPost.created_by(self.user)
+        other_posts = BlogPost.not_created_by(self.user)
         self.render_internal('blog/home.html',
+                              my_posts=my_posts,
+                              other_posts=other_posts,
                               new_post=self.uri_for(Blog.routes.get('new_post')))
 
     def show_post(self, post_id=None):
@@ -95,9 +99,12 @@ class NewPost(AppSection):
         content = self.request.get('content')
 
         if subject and content:
-            p = BlogPost(parent=self.user.key, subject=subject, content=content)
+            p = BlogPost(parent=self.user.key,
+                         subject=subject,
+                         content=content,
+                         created_by=self.user.key)
             p.put()
-            self.redirect_to(Blog.routes.get('show_post'), post_id=p.key.integer_id())
+            self.redirect_to(Blog.routes.get('show_post'), post_id=p.key.id())
         else:
             error = "Subject and content fields are required."
             post = {
@@ -118,11 +125,29 @@ class EditPost(AppSection):
             self.render_internal('blog/update-post.html',
                                   post=post,
                                   is_editing=True,
-                                  home=self.uri_for(Blog.routes.get('show_post'), post_id=post.key.integer_id()))
+                                  home=self.uri_for(Blog.routes.get('show_post'), post_id=post.key.id()))
         else:
             self.error(404)
             return
 
     def post(self, post_id=None):
-        print 'EditPost.post()'
-        pass
+        post = BlogPost.get_by_id(int(post_id), parent=self.user.key)
+
+        if post:
+            post.subject = self.request.get('subject')
+            post.content = self.request.get('content')
+
+            if post.subject and post.content:
+                post.put()
+                self.redirect_to(Blog.routes.get('show_post'), post_id=post.key.id())
+            else:
+                error = "Subject and content fields are required."
+                self.render("blog/update-post.html",
+                            post=post,
+                            is_editing=True,
+                            error=error,
+                            home=self.uri_for(Blog.routes.get('show_post'), post_id=post.key.id()))
+
+        else:
+            self.error(404)
+            return
--- app/models/blogpost.py
+++ app/models/blogpost.py
@@ -11,12 +11,21 @@ class BlogPost(ndb.Model):
     subject = ndb.StringProperty(required = True)
     content = ndb.TextProperty(required = True)
     created = ndb.DateTimeProperty(auto_now_add = True)
+    created_by = ndb.KeyProperty(required = True)
     last_modified = ndb.DateTimeProperty(auto_now = True)
     comments = ndb.StructuredProperty(Comment, repeated=True)
 
     def __init__(self, *args, **kwds):
         super(BlogPost, self).__init__(*args, **kwds)
         self.t_renderer = TemplateRenderer()
+
+    @classmethod
+    def created_by(cls, user):
+        return cls.query(ancestor=user.key).order(cls.created).fetch()
+
+    @classmethod
+    def not_created_by(cls, user):
+        return cls.query().filter(ndb.GenericProperty("created_by") != user.key).fetch()
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
